971756cc15c2d195fb1002f21f7a8a4494f6cc6d
Adam Leskis <leskis@gmail.com>
2022-07-26 22:47:43 +0100

    first commit of working app shamelessly copied from a tutorial on books crud app

---
+++ app/go.mod
@@ -0,0 +1,11 @@
+module code-corpus-api
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/jinzhu/gorm v1.9.16
+	gorm.io/driver/postgres v1.3.8
+	gorm.io/driver/sqlite v1.3.6
+	gorm.io/gorm v1.23.8
+)
---
+++ app/models/setup.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	// this host is for inside the compose network...should be parameterized
+	dsn := "host=db user=codez password=codez-control dbname=code port=5432 sslmode=disable"
+
+	database, err := gorm.Open(postgres.Open(dsn))
+
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	database.AutoMigrate(&Book{})
+
+	DB = database
+}
