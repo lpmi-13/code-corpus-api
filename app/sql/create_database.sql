e4c6b278c519e24514b1d4c0021981b116466a6a
Adam Leskis <leskis@gmail.com>
2022-11-03 22:10:19 +0000

    nvmd, we can totally just use a shell script that easily accepts environment vars

--- app/sql/create_database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE code;
- 
-CREATE USER codez WITH PASSWORD 'codezcontrol';
- 
-ALTER ROLE codez SET client_encoding TO 'utf8';
-ALTER ROLE codez SET default_transaction_isolation TO 'read committed';
-ALTER ROLE codez SET timezone TO 'UTC';
- 
-GRANT ALL PRIVILEGES ON DATABASE code TO codez;
